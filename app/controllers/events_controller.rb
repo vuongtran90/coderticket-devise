ec073ebf549e76c989fbcbc099f77af764af68c8
Vuong Tran <lifebreakvn@gmail.com>
2015-12-22 11:00:25 +0000

    Search and devise

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all    
+    @events = Event.where('starts_at >= ?', Date.today)
+    if params[:search]
+      @events = Event.search(params[:search]).order("created_at DESC")
+    else
+      @events = Event.all.order("created_at DESC")
+    end
   end
 
   def show
