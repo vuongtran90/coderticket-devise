9d4844c184ba405308fbff74dc14dfd6e1c49ecb
Vuong Tran <lifebreakvn@gmail.com>
2015-12-27 15:07:16 +0000

    add ruby ver

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224034037) do
+ActiveRecord::Schema.define(version: 20151223101813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,7 @@ ActiveRecord::Schema.define(version: 20151224034037) do
     t.string   "name"
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
-    t.datetime "public_at"
     t.boolean  "published",                 default: false
-    t.integer  "user_id"
   end
 
   add_index "events", ["category_id"], name: "index_events_on_category_id", using: :btree
